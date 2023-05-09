c7280863bd1d23a3825d875953c860a1894e2798
frchase <69663529+frchase@users.noreply.github.com>
2023-05-09 00:50:01 -0400

    finished

--- cli.js
+++ cli.js
@@ -74,14 +74,14 @@ var url = `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude
 var response = await fetch(url);
 const data = await response.json();
 
-const days = args.d 
+const days = args.d;
 
 if (days == 0) {
-  console.log("today.")
+  console.log("today.");
 } else if (days > 1) {
-  console.log("in " + days + " days.")
+  console.log("in " + days + " days.");
 } else {
-  console.log("tomorrow.")
+  console.log("tomorrow.");
 }
 
 if (args.j) {
